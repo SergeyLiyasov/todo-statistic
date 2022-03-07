0d8d76c6a6eaf61f568aa701458a7954817afd0f
SergeyLiyasov <liseand2002@gmail.com>
2022-03-07 16:32:36 +0500

    s

--- index.js
+++ index.js
@@ -11,10 +11,26 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getTODOs(file) {
+    let answer = [];
+    for (let row in file.split('\n')) {
+        const TODOBeginning = '// TODO ';
+        let i = row.lastIndexOf();
+        if (i < 0) {
+            continue;
+        }
+        answer.push(row.substring(i + TODOBeginning.length));
+    }
+    return answer;
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'todos':
+            console.log(getFiles().map(getTODOs));
             break;
         default:
             console.log('wrong command');
